6156c8b7cf5c68dbe478f56c12e0f37655c015e0
Mohit <mohitt.tech@gmail.com>
2025-11-29 02:20:59 +0530

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+struct Node* top = NULL;
+
+void push(int val) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = val;
+    temp->next = top;
+    top = temp;
+}
+
+int pop() {
+    struct Node* temp = top;
+    int val = temp->data;
+    top = top->next;
+    free(temp);
+    return val;
+}
+
+int main() {
+    char exp[50];
+    printf("Enter postfix expression: ");
+    scanf("%s", exp);
+
+    for (int i = 0; exp[i] != '\0'; i++) {
+        char ch = exp[i];
+        if (isdigit(ch))
+            push(ch - '0');
+        else {
+            int b = pop();
+            int a = pop();
+            switch (ch) {
+                case '+': push(a + b); break;
+                case '-': push(a - b); break;
+                case '*': push(a * b); break;
+                case '/': push(a / b); break;
+            }
+        }
+    }
+    printf("Result: %d\n", pop());
+    return 0;
+}
