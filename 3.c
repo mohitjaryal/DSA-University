a34b59d4906523cf813764679bc6d5c31e4dad77
Mohit <mohitt.tech@gmail.com>
2025-11-29 02:21:12 +0530

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++)
+        for (int j = 0; j < n - i - 1; j++)
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+}
+
+int linearSearch(int arr[], int n, int key) {
+    for (int i = 0; i < n; i++)
+        if (arr[i] == key) return i;
+    return -1;
+}
+
+int binarySearch(int arr[], int n, int key) {
+    int low = 0, high = n - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (arr[mid] == key) return mid;
+        else if (arr[mid] < key) low = mid + 1;
+        else high = mid - 1;
+    }
+    return -1;
+}
+
+int main() {
+    int arr[50], n, key;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    printf("Enter elements:\n");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    bubbleSort(arr, n);
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    int pos = linearSearch(arr, n, key);
+    if (pos != -1) printf("Found using linear search at %d\n", pos);
+    else printf("Not found using linear search\n");
+
+    pos = binarySearch(arr, n, key);
+    if (pos != -1) printf("Found using binary search at %d\n", pos);
+    else printf("Not found using binary search\n");
+
+    return 0;
+}
